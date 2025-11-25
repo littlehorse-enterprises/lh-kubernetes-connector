2f353e296172cb839d32188459f8ab18dcd2cb7a
Saúl Piña <sauljabin@gmail.com>
2025-11-24 19:45:17 -0500

    build: fix pipeline and docker file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-
 set -e
 
-mode=${1:-jvm}
+ARCH=$(arch)
 
-echo "Starting connector in '$mode' mode"
+if [[ "${ARCH}" != "x86_64" ]]; then
+  MODE="jvm"
+  echo "Starting connector in 'jvm' mode because 'native' is not supported for '${ARCH}'"
+else
+  MODE=${1:-"jvm"}
+  echo "Starting connector in '${MODE}' mode"
+fi
 
-if [[ ${mode,,} == "jvm" ]]; then
+if [[ "${MODE}" == "jvm" ]]; then
   java -Dquarkus.http.host=0.0.0.0 -Djava.util.logging.manager=org.jboss.logmanager.LogManager -jar /connector/quarkus-run.jar
-elif [[ ${mode,,} == "native" ]]; then
+elif [[ "${MODE}" == "native" ]]; then
   /connector/quarkus-run -Dquarkus.http.host=0.0.0.0
 else
-  echo "Invalid type, should be either 'native' or 'jvm'"
+  echo "Invalid type, should be either 'jvm' or 'native'"
   exit 1
 fi
